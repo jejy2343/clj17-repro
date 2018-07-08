a7a5239d269dd8e1dabb381b77b02acfac520d5f
default <default@localhost.localdomain>
2018-07-08 17:29:42 -0400

    .

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject om-official-tutorial "1.1.0-SNAPSHOT"
+  :description "My first Om program!"
+  :dependencies [#_[org.clojure/clojure "1.9.0"]
+                 #_[org.clojure/clojurescript "1.10.339"]
+                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha34"]
+                 [figwheel-sidecar "0.5.16" :scope "test"]])
